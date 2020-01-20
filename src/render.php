2140ef4e30d1d8837de50ea22c2ba945d54e9db1
MUU <urs.mueller@compass-security.com>
2020-01-20 06:46:34 -0800

    Added error message if file to be rendered does not exist.

--- src/render.php
+++ src/render.php
@@ -9,12 +9,24 @@ $smarty->setCacheDir('../smarty/cache');
 $smarty->setConfigDir('../smarty/configs');
 
 if ( isset($_POST["template"]) ) {
-    $smarty->display('../smarty/templates/' . $_POST["template"]);
+
+    $fpath = '../smarty/templates/' . basename($_POST["template"]); 
+    if ( file_exists($fpath) ) {
+        $smarty->display($fpath);
+    } else {
+        $html = <<<XYZ
+            <html>
+                <body>
+                    This file does not exist. Go back to the <a href="index.php"> index page</a>.
+                </body>
+            </html>
+        XYZ;
+        echo $html;
+    }
 } else {
     $host  = $_SERVER['HTTP_HOST'];
     $uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
     $extra = 'index.php';  // change accordingly
-
     header("Location: http://$host$uri/$extra");
 }
 
